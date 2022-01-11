b8b9ab82e723d5c6003844fdb3fd27f86b79f6c7
zanceballos <izzan2234@outlook.com>
2022-01-11 15:39:06 +0800

    Initial Commit

---
+++ clicnic.eclipse/src/main/java/com/clinicdevops/clicnic/eclipse/App.java
@@ -0,0 +1,13 @@
+package com.clinicdevops.clicnic.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
